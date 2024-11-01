8f2ab058aadf0f44025153046bd7516291311011
ismailchiba <ismailchiba0@gmail.com>
2024-11-01 13:31:13 +0100

    update

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -2,8 +2,7 @@ import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
-$('body').prepend('<div id="logo"></div>')
-  .append('<p>Holberton Dashboard</p>')
+$('body').append('<p>Holberton Dashboard</p>')
   .append('<p>Dashboard data for the students</p>')
   .append('<button>Click here to get started</button>')
   .append('<p id="count"></p>')
