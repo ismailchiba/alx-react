3d0f4c314c7d6cb968d92881fbd5950bc4438f4f
ismailchiba <ismailchiba0@gmail.com>
2024-11-03 20:53:44 +0100

    test

--- 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x01-react_intro/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Notifications from "..Notifications/Notifications";
+import Notifications from "../Notifications/Notifications";
 
 describe("Notification component tests", () => {
   it("renders Notification component without crashing", () => {
