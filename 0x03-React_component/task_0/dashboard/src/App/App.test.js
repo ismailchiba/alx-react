400d5f22ba67e362a29361c66775d7f477d7bf0a
ismailchiba <ismailchiba0@gmail.com>
2024-11-15 15:13:48 +0100

    add testing

--- 0x03-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import App from "./App";
 import { shallow } from "enzyme";
 
-describe("App Componeent Tests", () => {
+describe("", () => {
   it("Renders without crashing", () => {
     const app = shallow(<App />);
     expect(app).toBeDefined();
