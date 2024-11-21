5ae855c8067d660886a0e61fbba822eb2ea24f9b
ismailchiba <ismailchiba0@gmail.com>
2024-11-21 22:24:15 +0100

    update

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,7 @@
 import { fromJS } from 'immutable';
 
-export default function getImmutableObject (object) {
+function getImmutableObject (object) {
   return fromJS(object);
 }
+
+export default getImmutableObject;
